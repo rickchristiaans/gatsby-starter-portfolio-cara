21b5c4d3034ca15180b0e88a9291c8f2eed95851
rickchristiaans <40057950+rickchristiaans@users.noreply.github.com>
2021-03-07 23:24:11 +0100

    Update footer.jsx

--- src/@lekoarts/gatsby-theme-cara/components/footer.jsx
+++ src/@lekoarts/gatsby-theme-cara/components/footer.jsx
@@ -1,32 +1,3 @@
 /** @jsx jsx */
-import { Box, Flex, Link, jsx } from "theme-ui"
 
-const Footer = <Box as="footer" variant="footer">
-      <Flex
-        sx={{
-          justifyContent: `center`,
-          alignItems: `center`,
-          mt: 3,
-          color: `text`,
-          fontWeight: `semibold`,
-          a: { color: `text` },
-        }}
-      >
-        <img width="30" height="30" src="https://img.lekoarts.de/gatsby/logo_w30.png" alt="LekoArts Logo" />
-        {` `}
-        <Link
-          aria-label="Link to the theme's GitHub repository"
-          sx={{ ml: 2 }}
-          href="https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-cara"
-        >
-          Theme
-        </Link>
-        <div sx={{ mx: 1 }}>by</div>
-        {` `}
-        <Link aria-label="Link to the theme author's website" href="https://www.lekoarts.de/en">
-          LekoArts
-        </Link>
-      </Flex>
-    </Box>
-
-export default Footer
+const Footer = <h1>Hello, world!</h1>;
