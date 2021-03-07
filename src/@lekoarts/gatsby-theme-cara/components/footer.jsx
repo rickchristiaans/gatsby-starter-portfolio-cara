0edbbb427df4c7899ab2f4ad3ede85b6426d7361
rickchristiaans <40057950+rickchristiaans@users.noreply.github.com>
2021-03-07 22:49:35 +0100

    Update footer.jsx

--- src/@lekoarts/gatsby-theme-cara/components/footer.jsx
+++ src/@lekoarts/gatsby-theme-cara/components/footer.jsx
@@ -1,3 +1,14 @@
-<Box as="footer" variant="footer">
+/** @jsx jsx */
+import { Box, Flex, Link, jsx } from "theme-ui"
+
+const Footer = () => {
+  }
+
+  return (
+    <Box as="footer" variant="footer">
       Copyright &copy; {new Date().getFullYear()}. All rights reserved.
     </Box>
+  )
+}
+
+export default Footer
